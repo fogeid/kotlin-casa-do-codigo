3a5147c79685f6fa665af0999b1bf2527db88579
Diego Batista <diego.silva@aluno.ic.ufmt.br>
2021-08-17 09:06:09 -0400

    feat: implements method PUT autor controller

--- src/main/kotlin/br/com/zup/controllers/AutorController.kt
+++ src/main/kotlin/br/com/zup/controllers/AutorController.kt
@@ -4,11 +4,10 @@ import br.com.zup.dto.request.AutorRequest
 import br.com.zup.dto.response.AutorResponse
 import br.com.zup.repositories.AutorRepository
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.*
+import io.micronaut.http.uri.UriBuilder
 import io.micronaut.validation.Validated
+import javax.transaction.Transactional
 import javax.validation.Valid
 
 @Validated
@@ -16,12 +15,17 @@ import javax.validation.Valid
 class AutorController(val autorRepository: AutorRepository) {
 
     @Post
-    fun inserir(@Body @Valid request: AutorRequest) {
+    @Transactional
+    fun inserir(@Body @Valid request: AutorRequest): HttpResponse<Any> {
         val autor = request.toModel()
         autorRepository.save(autor)
+        val uri = UriBuilder.of("/autores/{id}")
+            .expand(mutableMapOf(Pair("id", autor.id)))
+        return HttpResponse.created(uri)
     }
 
     @Get
+    @Transactional
     fun lista(): HttpResponse<List<AutorResponse>> {
         val autores = autorRepository.findAll()
         val response = autores.map {
@@ -29,6 +33,21 @@ class AutorController(val autorRepository: AutorRepository) {
         }
 
         return HttpResponse.ok(response)
+    }
+
+    @Put("/{id}")
+    @Transactional
+    fun atualiza(@PathVariable id: String, descricao: String): HttpResponse<Any> {
+        val possivelAutor = autorRepository.findById(id)
+
+        if (possivelAutor.isEmpty) {
+            return HttpResponse.notFound()
+        }
+
+        val autor = possivelAutor.get()
+        autor.descricao = descricao
+        autorRepository.update(autor)
+        return HttpResponse.ok(AutorResponse(autor))
     }
 
 }
